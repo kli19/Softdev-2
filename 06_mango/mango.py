5bfd08175092cec6181a2356fa22db5e3a91bd90
Karen Li <kli19@stuy.edu>
2019-02-28 23:23:30 -0500

    borough

--- 06_mango/mango.py
+++ 06_mango/mango.py
@@ -2,3 +2,25 @@
 #SoftDev2 pd6
 #K06 -- Yummy Mango Py
 #2019-02-28
+
+import pymongo
+
+SERVER_ADDR="142.93.206.119"
+connection=pymongo.MongoClient(SERVER_ADDR)
+db=connection.test
+collection = db.restaurants
+
+#All restaurants in a specified borough.
+def get_borough(borough):
+    return collection.find( {'borough': borough} )
+
+def display_restaurants(collection):
+    for i in collection:
+        #print (i['name'])
+        print(i)
+
+print ("-----RESTAURANTS IN QUEENS-----\n")
+#display_restaurants(get_borough("Queens"))
+
+
+
