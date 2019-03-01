2e81f3bead5feaafb7baa0576cb43c18200ef015
Karen Li <kli19@stuy.edu>
2019-02-28 23:29:42 -0500

    zipcode and score

--- 06_mango/mango.py
+++ 06_mango/mango.py
@@ -34,6 +34,10 @@ def get_zipcode(zipcode):
 def get_zip_grade(zipcode, grade):
     return collection.find( {'address.zipcode': zipcode, 'grades.grade': grade} )
 
+#All restaurants in a specified zip code with a score below a specified threshold.
+def get_zip_score(zipcode, score):
+    return collection.find( {'address.zipcode': zipcode, 'grades.score': {'$lt': score} } )
+
 def display_restaurants(collection):
     for i in collection:
         #print (i['name'])
@@ -48,7 +52,8 @@ print ("-----RESTAURANTS IN 10282----\n")
 print ("-----RESTAURANTS IN 10282 WITH GRADE A----\n")
 #display_restaurants(get_zip_grade("10282", "A"))
 
-
+print ("-----RESTAURANTS IN 10282 WITH SCORE LOWER THAN 7----\n")
+#display_restaurants(get_zip_score("10282", 5))
 
 
 
