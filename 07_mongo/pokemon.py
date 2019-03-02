6d3c7ba78c065e25ad7260af343e1fee8db19ace
Karen Li <kli19@stuy.edu>
2019-03-02 15:45:08 -0500

    add dataset

---
+++ 07_mongo/pokemon.py
@@ -0,0 +1,8 @@
+#HeartGold -- Karen Li, Thomas Lee
+#SoftDev2 pd6
+#K07 -- Import/Export Bank
+#2019-03-02
+
+import pymongo
+SERVER_ADDR="142.93.206.119"
+connection=pymongo.MongoClient(SERVER_ADDR)
