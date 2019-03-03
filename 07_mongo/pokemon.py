a74f2ccc99197092e9a8b35ff7146b35511222a6
Karen Li <kli19@stuy.edu>
2019-03-02 21:06:05 -0500

    type and weakness

--- 07_mongo/pokemon.py
+++ 07_mongo/pokemon.py
@@ -20,3 +20,27 @@ def find_id(pokemon_id):
 
 def find_name(name):
     return collection.find({"name": name})
+
+def find_type(pokemon_type):
+    return collection.find({"type": pokemon_type})
+
+def find_weakness(weakness):
+    return collection.find({"weaknesses": weakness})
+
+def find_type_weakness(pokemon_type, weakness):
+    return collection.find({"$and":[{"type": pokemon_type}, {"weaknesses": weakness}]}) 
+
+print("=====POKEMON WITH ID 1=====")
+display_pokemon(find_id(1))
+
+print("=====POKEMON WITH NAME Jigglypuff=====")
+display_pokemon(find_name("Jigglypuff"))
+
+print("=====POKEMON WITH TYPE GRASS=====")
+display_pokemon(find_type("Grass"))
+
+print("=====POKEMON WITH WEAKNESS WATER=====")
+display_pokemon(find_weakness("Water"))
+
+print("=====POKEMON WITH TYPE DRAGON AND WEAKNESS DRAGON=====")
+display_pokemon(find_type_weakness("Dragon", "Dragon"))
