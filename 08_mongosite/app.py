a121a43b12e355196d1d793db869654c7ed8b089
Karen Li <kli19@stuy.edu>
2019-03-07 21:05:48 -0500

    simplified code; don't need util

--- 08_mongosite/app.py
+++ 08_mongosite/app.py
@@ -3,14 +3,23 @@
 #K08 -- Ay Mon, Go Git It From Yer Flask
 #2019-03-02
 
-import os
+import os, pymongo, json
 
 from flask import Flask, render_template, request
-from util import pokemon
 
 app = Flask(__name__)
 
 app.secret_key = os.urandom(32)
+
+SERVER_ADDR="142.93.206.119"
+connection=pymongo.MongoClient(SERVER_ADDR)
+connection.drop_database("HeartGold")
+db = connection.HeartGold
+collection = db.pokemon
+
+f = open("data/pokedex.json")
+data = json.load(f)
+collection.insert_many(data["pokemon"])
 
 @app.route("/", methods=["POST", "GET"])
 def home():
@@ -19,7 +28,7 @@ def home():
 @app.route("/poke_info", methods=["POST", "GET"])
 def poke_info():
     poke_name = request.args["poke_name"]
-    poke_dict = pokemon.find_name(poke_name)
+    poke_dict = collection.find_one({"name": poke_name})
     if poke_dict is None:
         poke_dict = {}
     length = len(poke_dict)
@@ -29,11 +38,22 @@ def poke_info():
 @app.route("/ip", methods=["POST", "GET"])
 def change_ip():
     ip = request.args["ip_address"]
-    print(pokemon.SERVER_ADDR)
-    pokemon.change_ip(ip)
-    #print(pokemon.SERVER_ADDR)
+    print(SERVER_ADDR)
+
+    SERVER_ADDR= ip
+    connection=pymongo.MongoClient(SERVER_ADDR)
+    connection.drop_database("HeartGold")
+    db = connection.HeartGold
+    collection = db.pokemon
+    
+    f = open("data/pokedex.json")
+    data = json.load(f)
+    collection.insert_many(data["pokemon"])
+    
+    print(SERVER_ADDR)
     return render_template("change_ip.html", ip = ip)
 
 if __name__ == "__main__":
     app.debug = True #change to False before our demo
     app.run()
+    #app.run("0.0.0.0")
