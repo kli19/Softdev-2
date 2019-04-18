8b1d6ff01fb0f7475b62c5eff4f3f70d2f247bf7
Karen Li <kli19@stuy.edu>
2019-04-18 12:13:28 -0400

    comment out print

--- 20_anon-reduce/thing.py
+++ 20_anon-reduce/thing.py
@@ -10,23 +10,22 @@ from functools import reduce
 f = open("ulysses.txt", "r")
 data = f.read().split()
 #data = ["hi", "the", "the", "and"]
-print(data)
+#print(data)
 
 def freq(word):
     count = [1 if x == word else 0 for x in data]
     return reduce((lambda x, y: x + y),count)
-print(freq("the"))
-print(data.count("the"))
+#print(freq("the"))
 
 def freq_group(words):
     num = 1 + words.count(" ")
     count = [1 if reduce((lambda x, y: x + " " + y), data[i: i+num]) == words else 0 for i in range(0, len(data)) ]
     return reduce((lambda x, y: x + y), count)
-print(freq_group("he and"))
+#print(freq_group("he and"))
 
     
 def freq_most():
     words = set(data)
     count = [(x, freq(x)) for x in words]
     return reduce((lambda x, y: x if x[1] > y[1] else y), count)
-print(freq_most())
+#print(freq_most())
